c5acf9baf40a00a6f59ddd084a9ec2950e1df342
husam <husam@mail.com>
2024-12-02 15:10:29 +0100

    modified the design of break sessions

--- lib/screens/sessions/session_tile.dart
+++ lib/screens/sessions/session_tile.dart
@@ -17,53 +17,97 @@ class SessionTile extends StatelessWidget {
     super.key,
     required this.session,
     required this.conference,
-    required this.sessionIncrement, required this.dayIncrement,
+    required this.sessionIncrement,
+    required this.dayIncrement,
   });
 
   @override
   Widget build(BuildContext context) {
     DateTime now = DateTime.now();
 
-    // Determine the status of the session based on the current time
-    Color circleColor;
-    if (now.isBefore(session.startTime)) {
-      circleColor = Colors.orange; // Session is upcoming
-    } else if (now.isAfter(session.endTime)) {
-      circleColor = Colors.red; // Session has ended
-    } else {
-      circleColor = Colors.green; // Session is ongoing
-    }
-
-    // Use DateFormat to format the start and end times
-    String formattedStartTime = DateFormat('hh:mm a').format(session.startTime);
-    String formattedEndTime = DateFormat('hh:mm a').format(session.endTime);
-
-    return FutureProvider<List<Person>?>(
-      create: (context) =>
-          DatabaseService(uid: 'uid').fetchChairPersons(session.chairPersons),
-      initialData: null,
-      child: Card(
-        margin: const EdgeInsets.fromLTRB(20.0, 6.0, 20.0, 0.0),
-        child: Column(
-          children: [
-            ListTile(
-              leading: CircleAvatar(
-                radius: 25.0,
-                backgroundColor:
-                    circleColor, // Set the color based on the status
+    if (session.isBreak) {
+      // Design for break sessions
+      return Card(
+          margin: const EdgeInsets.fromLTRB(20.0, 6.0, 20.0, 0.0),
+          color: Colors.lightGreen[200],
+          child: ListTile(
+              leading: const Icon(
+                Icons.coffee,
+                color: Colors.brown,
+                size: 30.0,
               ),
               title: Text(
-                "Session $sessionIncrement: \n ${session.title}",
-                style: const TextStyle(fontWeight: FontWeight.bold),
+                session.title,
+                style: const TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 18.0,
+                ),
               ),
-              subtitle: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text('$formattedStartTime - $formattedEndTime'),
-                  Text(session.location),
-                  const SizedBox(height: 8.0), // Space between lines
-                  Consumer<List<Person>?>(
-                    builder: (context, chairPersons, child) {
+              subtitle: Text(
+                "Location: ${session.location}",
+                style: const TextStyle(fontStyle: FontStyle.italic),
+              ),
+              onTap: () {
+                // Show the description in a popup
+                showDialog(
+                  context: context,
+                  builder: (BuildContext context) {
+                    return AlertDialog(
+                      title: const Text("Break Details"),
+                      content: Text(session.description),
+                      actions: [
+                        TextButton(
+                          onPressed: () {
+                            Navigator.of(context).pop(); // Close the dialog
+                          },
+                          child: const Text("Close"),
+                        ),
+                      ],
+                    );
+                  },
+                );
+              }));
+    } else {
+      // Regular session design
+      Color circleColor;
+      if (now.isBefore(session.startTime)) {
+        circleColor = Colors.orange; // Session is upcoming
+      } else if (now.isAfter(session.endTime)) {
+        circleColor = Colors.red; // Session has ended
+      } else {
+        circleColor = Colors.green; // Session is ongoing
+      }
+
+      String formattedStartTime =
+          DateFormat('hh:mm a').format(session.startTime);
+      String formattedEndTime = DateFormat('hh:mm a').format(session.endTime);
+
+      return FutureProvider<List<Person>?>(
+        create: (context) =>
+            DatabaseService(uid: 'uid').fetchChairPersons(session.chairPersons),
+        initialData: null,
+        child: Card(
+          margin: const EdgeInsets.fromLTRB(20.0, 6.0, 20.0, 0.0),
+          child: Column(
+            children: [
+              ListTile(
+                leading: CircleAvatar(
+                  radius: 25.0,
+                  backgroundColor:
+                      circleColor, // Set the color based on the status
+                ),
+                title: Text(
+                  session.title,
+                  style: const TextStyle(fontWeight: FontWeight.bold),
+                ),
+                subtitle: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text('$formattedStartTime - $formattedEndTime'),
+                    Text(session.location),
+                    const SizedBox(height: 8.0),
+                    Consumer<List<Person>?>(
+                        builder: (context, chairPersons, child) {
                       if (chairPersons == null) {
                         return const Center(child: CircularProgressIndicator());
                       } else if (chairPersons.isEmpty) {
@@ -79,26 +123,26 @@ class SessionTile extends StatelessWidget {
                           );
                         }).toList(),
                       );
-                    },
-                  ),
-                ],
+                    }),
+                  ],
+                ),
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => PapersScreen.session(
+                          session: session,
+                          conference: conference,
+                          dayIncrement: dayIncrement,
+                          sessionIncrement: sessionIncrement),
+                    ),
+                  );
+                },
               ),
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => PapersScreen.session(
-                        session: session,
-                        conference: conference,
-                        dayIncrement: dayIncrement,
-                        sessionIncrement: sessionIncrement),
-                  ),
-                );
-              },
-            ),
-          ],
+            ],
+          ),
         ),
-      ),
-    );
+      );
+    }
   }
 }
